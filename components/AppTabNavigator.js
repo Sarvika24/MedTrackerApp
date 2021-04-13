d95f33071450ab95cc5a5e2a890d0c52757ceb3d
Sarvika24 <63595437+Sarvika24@users.noreply.github.com>
2021-04-13 18:08:26 +0530

    Add files via upload

---
+++ components/AppTabNavigator.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import SosScreen from '../screens/SosScreen';
+import BookRequestScreen from '../screens/BookRequestScreen';
+
+
+
+export const AppTabNavigator = createBottomTabNavigator({
+  SosScreen : {
+    screen: SosScreen,
+    navigationOptions :{
+      tabBarIcon : <Image source={require("../assets/sos1.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "SOS",
+    }
+  },
+  BookRequest: {
+    screen: BookRequestScreen,
+    navigationOptions :{
+      tabBarIcon : <Image source={require("../assets/request-book.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "Book Request",
+    }
+  }
+});
